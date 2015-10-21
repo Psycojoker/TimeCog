91bdcbc7c14bd69c7811d5a04f4910366fab9731
Laurent Peuch <cortex@worlddomination.be>
2015-10-21 11:01:55 +0200

    [mod] I want to store the kind of event

--- events/models.py
+++ events/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Event(models.Model):
     organisation = models.CharField(max_length=255)
 
+    kind = models.CharField(max_length=255)
+
     title = models.CharField(max_length=255)
     date = models.DateField()
     time = models.TimeField(null=True, blank=True)
